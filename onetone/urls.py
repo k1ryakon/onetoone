3a343f9741edf49a2a419427b385d509ec925f81
SAVELO <papush367@gmail.com>
2024-12-24 21:02:22 +0300

    123

---
+++ onetone/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from onetone import views
+
+
+urlpatterns = [
+    path('', views.index),
+    path('create/', views.create),
+    path('edit/<int:id>/', views.edit),
+    path('delete/<int:id>/', views.delete),
+]
